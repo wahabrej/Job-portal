474f193ca4384e9a76e479b293c82c9195f661a0
Abdul Wahab <awahabreja@gmail.com>
2024-07-17 09:59:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Header from "./Component/Header";
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+// git remote add origin https://github.com/wahabrej/Job-grooming.git
+// git branch -M main
+// git push -u origin main
